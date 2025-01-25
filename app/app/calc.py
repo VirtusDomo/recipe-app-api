697a5e6cd0a564ceb065f485bca94e881bd5b7e6
VirtusDomo <abanybe@gmail.com>
2025-01-25 06:07:35 -0600

    Linting fixes

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,12 +1,13 @@
-'''
+"""
 Calculator Functions
-'''
+"""
 
 
-def add(x ,y):
-    '''Add x and y and return result.'''
+def add(x, y):
+    """Add x and y and return result."""
     return x + y
 
-def subtract(x ,y):
-    '''Subtract x and y and return result.'''
+
+def subtract(x, y):
+    """Subtract x and y and return result."""
     return y - x
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -9,10 +9,10 @@ from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
+
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
     '''Test Commands'''
-
 
     def test_wait_for_db_ready(self, patched_check):
         '''Test waiting for if database is ready'''
